45d68dd38a92c74739dcaa70918b8618da11ab25
Ophecy <arthur.de.berranger@gmail.com>
2020-09-29 15:08:54 +0200

    fix(test): fixed test and test attributes/methods

--- common/src/main/java/chatProject/model/messages/Message.java
+++ common/src/main/java/chatProject/model/messages/Message.java
@@ -6,6 +6,7 @@ import chatProject.model.user.UserInfo;
 /**
  * A model for a message sent in the chat.
  * A message has a content (of a generic type T), a unique ID and a sender.
+ *
  * @param <T> the type of messages in the chat
  */
 public class Message<T> {
@@ -31,14 +32,16 @@ public class Message<T> {
 
     /**
      * Gets the ID of the message.
+     *
      * @return the message unique ID
      */
     public int getId() {
-        return 0;
+        return id;
     }
 
     /**
      * Gets the sender of the message.
+     *
      * @return the user who sent the message
      */
     public UserInfo getSender() {
@@ -47,6 +50,7 @@ public class Message<T> {
 
     /**
      * Gets the content of the message.
+     *
      * @return the message content
      */
     public T getMessage() {
